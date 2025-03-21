b8679af66d47368b48ca73a3c0a9cbd6132b8033
zackcole <183650880+zackcole@users.noreply.github.com>
2025-03-20 23:40:18 -0700

    update route logout modal

--- src/component/authentication/LogOutModal.jsx
+++ src/component/authentication/LogOutModal.jsx
@@ -5,8 +5,8 @@ const LogOutModal = ({logOutModal,setLogOutModal}) => {
   return (
     logOutModal && (
       <div className="w-screen h-screen flex items-center justify-center bg-[rgba(0,0,0,0.4)] fixed top-0 left-0 right-0 bottom-0 z-30 px-4">
-        <div className="bg-[#FFFFFF] backdrop-blur-[100px] w-[440px] py-10  rounded-[12px] ">
-          <div className="flex items-start flex-col px-5 justify-center gap-y-4 h-full">
+        <div className="bg-[#FFFFFF] backdrop-blur-[100px] w-[410px] py-5  rounded-[12px] ">
+          <div className="flex items-start flex-col px-8 justify-center gap-y-4 h-full">
             <div  className="flex flex-col gap-y-2 items-start" >
               {/* <img src={Checkbox} alt="" /> */}
               <h3 className="text-2xl text-[#181818]" >Log Out</h3>
@@ -18,7 +18,7 @@ const LogOutModal = ({logOutModal,setLogOutModal}) => {
             }} className="bg-[#21293514] text-[#212935] text-sm px-4 py-1 rounded-[14px] w-[140px] h-[44px]">No</button>
             <button onClick={()=>{  
                setLogOutModal(!logOutModal)                          
-              navigate("/auth/login")                             
+              navigate("/login")                             
             }}  className="w-[140px] h-[44px] rounded-[14px] bg-[#EE3131] text-white flex gap-2 items-center justify-center text-md font-medium">Yes</button>
             
           </div>
